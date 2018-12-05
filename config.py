b40506aff11cd7884069f1282107521ea6b4892e
Kuyi King <kuyiking@gmail.com>
2018-12-05 15:35:13 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,47 +1,55 @@
+# -*- coding: utf-8 -*-
+
 import os
 import tensorflow as tf
 
-'''
-This file is taken and modified from R-Net by HKUST-KnowComp
-https://github.com/HKUST-KnowComp/R-Net
-'''
+from prepare import prepare
+from main import train, test
+import tensorflow as tf
 
-from prepro import prepro
-from main import train, test, demo
 
-flags = tf.flags
+# Set directory
+###################################################################################################################
+# home = os.getcwd()
+train_file = "/users/Xbc/QANet/chn/QANet/data/备用数据/gen_train.json"
+dev_file = "/users/Xbc/QANet/chn/QANet/data/备用数据/gen_dev.json"
+test_file = "/users/Xbc/QANet/chn/QANet/data/备用数据/gen_test.json"
+gpu_ratio, gpu_id = 0.5, "3"
 
-home = os.getcwd()
-train_file = os.path.join(home, "datasets", "squad", "train-v1.1.json")
-dev_file = os.path.join(home, "datasets", "squad", "dev-v1.1.json")
-test_file = os.path.join(home, "datasets", "squad", "dev-v1.1.json")
-glove_word_file = os.path.join(home, "datasets", "glove", "glove.840B.300d.txt")
-
+# 输出文件存放路径
 train_dir = "train"
-model_name = "FRC"
+model_name = "QANets_basic"
 dir_name = os.path.join(train_dir, model_name)
-if not os.path.exists(train_dir):
-    os.mkdir(train_dir)
-if not os.path.exists(os.path.join(os.getcwd(),dir_name)):
-    os.mkdir(os.path.join(os.getcwd(),dir_name))
-target_dir = "data"
+# 日志路径
 log_dir = os.path.join(dir_name, "event")
+# 模型路径
 save_dir = os.path.join(dir_name, "model")
+# 答案路径
 answer_dir = os.path.join(dir_name, "answer")
+answer_file = os.path.join(answer_dir, "answer.json")
+# 输入数据路径
+target_dir = "data"
 train_record_file = os.path.join(target_dir, "train.tfrecords")
 dev_record_file = os.path.join(target_dir, "dev.tfrecords")
 test_record_file = os.path.join(target_dir, "test.tfrecords")
-word_emb_file = os.path.join(target_dir, "word_emb.json")
-char_emb_file = os.path.join(target_dir, "char_emb.json")
+# 输出结果路径
 train_eval = os.path.join(target_dir, "train_eval.json")
 dev_eval = os.path.join(target_dir, "dev_eval.json")
 test_eval = os.path.join(target_dir, "test_eval.json")
 dev_meta = os.path.join(target_dir, "dev_meta.json")
 test_meta = os.path.join(target_dir, "test_meta.json")
+# 词典路径
+word_emb_file = os.path.join(target_dir, "word_emb.json")
+char_emb_file = os.path.join(target_dir, "char_emb.json")
 word_dictionary = os.path.join(target_dir, "word_dictionary.json")
 char_dictionary = os.path.join(target_dir, "char_dictionary.json")
-answer_file = os.path.join(answer_dir, "answer.json")
+pretrain_word_emb_file = "/users/zhuyujin/datasets/Tencent_AILab_ChineseEmbedding.txt"
+pretrain_char_emb_file = "/users/zhuyujin/datasets/Tencent_AILab_ChineseEmbedding.txt"
 
+if not os.path.exists(train_dir):
+    os.mkdir(train_dir)
+if not os.path.exists(os.path.join(os.getcwd(), dir_name)):
+    os.mkdir(os.path.join(os.getcwd(), dir_name))
 if not os.path.exists(target_dir):
     os.makedirs(target_dir)
 if not os.path.exists(log_dir):
@@ -51,45 +59,60 @@ if not os.path.exists(save_dir):
 if not os.path.exists(answer_dir):
     os.makedirs(answer_dir)
 
-flags.DEFINE_string("mode", "train", "Running mode train/debug/test")
+def set_gpu(gpu_ratio=0.3, gpu_id="0"):
+    os.environ["CUDA_VISIBLE_DEVICES"] = gpu_id
+    config = tf.ConfigProto() 
+    config.gpu_options.per_process_gpu_memory_fraction = gpu_ratio 
+    session = tf.Session(config=config)
+    return None
 
+    
+# Set flags
+###################################################################################################################
+flags = tf.flags
+
+# settings for files
+flags.DEFINE_string("mode", "train", "Running mode train/debug/test")
 flags.DEFINE_string("target_dir", target_dir, "Target directory for out data")
 flags.DEFINE_string("log_dir", log_dir, "Directory for tf event")
 flags.DEFINE_string("save_dir", save_dir, "Directory for saving model")
+
 flags.DEFINE_string("train_file", train_file, "Train source file")
 flags.DEFINE_string("dev_file", dev_file, "Dev source file")
 flags.DEFINE_string("test_file", test_file, "Test source file")
-flags.DEFINE_string("glove_word_file", glove_word_file, "Glove word embedding source file")
-
 flags.DEFINE_string("train_record_file", train_record_file, "Out file for train data")
 flags.DEFINE_string("dev_record_file", dev_record_file, "Out file for dev data")
 flags.DEFINE_string("test_record_file", test_record_file, "Out file for test data")
-flags.DEFINE_string("word_emb_file", word_emb_file, "Out file for word embedding")
-flags.DEFINE_string("char_emb_file", char_emb_file, "Out file for char embedding")
 flags.DEFINE_string("train_eval_file", train_eval, "Out file for train eval")
 flags.DEFINE_string("dev_eval_file", dev_eval, "Out file for dev eval")
 flags.DEFINE_string("test_eval_file", test_eval, "Out file for test eval")
 flags.DEFINE_string("dev_meta", dev_meta, "Out file for dev meta")
 flags.DEFINE_string("test_meta", test_meta, "Out file for test meta")
 flags.DEFINE_string("answer_file", answer_file, "Out file for answer")
+
+flags.DEFINE_string("pretrain_word_emb_file", pretrain_word_emb_file, "Input file for word embedding")
+flags.DEFINE_string("pretrain_char_emb_file", pretrain_char_emb_file, "Input file for char embedding")
+flags.DEFINE_string("word_emb_file", word_emb_file, "Out file for word embedding")
+flags.DEFINE_string("char_emb_file", char_emb_file, "Out file for char embedding")
 flags.DEFINE_string("word_dictionary", word_dictionary, "Word dictionary")
 flags.DEFINE_string("char_dictionary", char_dictionary, "Character dictionary")
 
-
-flags.DEFINE_integer("glove_char_size", 94, "Corpus size for Glove")
-flags.DEFINE_integer("glove_word_size", int(2.2e6), "Corpus size for Glove")
-flags.DEFINE_integer("glove_dim", 300, "Embedding dimension for Glove")
-flags.DEFINE_integer("char_dim", 64, "Embedding dimension for char")
-
+# settings for embeddings
+flags.DEFINE_boolean("use_char_emb", False, "Character dictionary")  # 是否使用字符embedding
 flags.DEFINE_integer("para_limit", 400, "Limit length for paragraph")
 flags.DEFINE_integer("ques_limit", 50, "Limit length for question")
 flags.DEFINE_integer("ans_limit", 30, "Limit length for answers")
 flags.DEFINE_integer("test_para_limit", 1000, "Limit length for paragraph in test file")
 flags.DEFINE_integer("test_ques_limit", 100, "Limit length for question in test file")
+flags.DEFINE_integer("test_ans_limit", 100, "Limit length for question in test file")
+
+flags.DEFINE_integer("char_dim", 64, "Embedding dimension for char")
+flags.DEFINE_integer("vec_size", 200, "Embedding dimension for char")
 flags.DEFINE_integer("char_limit", 16, "Limit length for character")
 flags.DEFINE_integer("word_count_limit", -1, "Min count for word")
 flags.DEFINE_integer("char_count_limit", -1, "Min count for char")
 
+# settings for model
 flags.DEFINE_integer("capacity", 15000, "Batch size of dataset shuffle")
 flags.DEFINE_integer("num_threads", 4, "Number of threads in input pipeline")
 flags.DEFINE_boolean("is_bucket", False, "build bucket batch iterator or not")
@@ -109,22 +132,14 @@ flags.DEFINE_integer("hidden", 96, "Hidden size")
 flags.DEFINE_integer("num_heads", 1, "Number of heads in self attention")
 flags.DEFINE_integer("early_stop", 10, "Checkpoints for early stop")
 
-# Extensions (Uncomment corresponding code in download.sh to download the required data)
-glove_char_file = os.path.join(home, "data", "glove", "glove.840B.300d-char.txt")
-flags.DEFINE_string("glove_char_file", glove_char_file, "Glove character embedding source file")
-flags.DEFINE_boolean("pretrained_char", False, "Whether to use pretrained character embedding")
-
-fasttext_file = os.path.join(home, "data", "fasttext", "wiki-news-300d-1M.vec")
-flags.DEFINE_string("fasttext_file", fasttext_file, "Fasttext word embedding source file")
-flags.DEFINE_boolean("fasttext", False, "Whether to use fasttext")
-
 
 def main(_):
+    set_gpu()
     config = flags.FLAGS
     if config.mode == "train":
         train(config)
-    elif config.mode == "prepro":
-        prepro(config)
+    elif config.mode == "prepare":
+        prepare(config)
     elif config.mode == "debug":
         config.num_steps = 2
         config.val_num_batches = 1
@@ -133,8 +148,6 @@ def main(_):
         train(config)
     elif config.mode == "test":
         test(config)
-    elif config.mode == "demo":
-        demo(config)
     else:
         print("Unknown mode")
         exit(0)
