d075a93ac23a701160133ac6cc43e6b50c9af77b
Kuyi King <kuyiking@gmail.com>
2018-12-05 15:36:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from tqdm import tqdm
 import ujson as json
 import tensorflow as tf
 
-from .model import Model
-from .util import get_record_parser, get_dataset, get_batch_dataset, convert_tokens, evaluate, evaluate_batch
+from model import Model
+from util import get_record_parser, get_dataset, get_batch_dataset, convert_tokens, evaluate, evaluate_batch
 
 
 def train(config):
@@ -21,8 +21,8 @@ def train(config):
         dev_eval_file = json.load(fh)
     with open(config.dev_meta, "r") as fh:
         meta = json.load(fh)
-
     dev_total = meta["total"]
+    
     print("Building model...")
     parser = get_record_parser(config)
     graph = tf.Graph()
@@ -41,10 +41,11 @@ def train(config):
         sess_config = tf.ConfigProto(allow_soft_placement=True)
         sess_config.gpu_options.allow_growth = True
 
+        # 初始化用于early-stopping的评价条件
         patience = 0
         best_f1 = 0.
         best_em = 0.
-
+        
         with tf.Session(config=sess_config) as sess:
             writer = tf.summary.FileWriter(config.log_dir)
             sess.run(tf.global_variables_initializer())
@@ -53,27 +54,29 @@ def train(config):
             dev_handle = sess.run(dev_iterator.string_handle())
             if os.path.exists(os.path.join(config.save_dir, "checkpoint")):
                 saver.restore(sess, tf.train.latest_checkpoint(config.save_dir))
-            global_step = max(sess.run(model.global_step), 1)
+            global_step = max(sess.run(model.global_step), 1)  # 初始化全局步长
 
-            for _ in tqdm(range(global_step, config.num_steps + 1)):
+            for _ in tqdm(range(global_step, config.num_steps + 1)):  # 训练模型
                 global_step = sess.run(model.global_step) + 1
-                # 运行模型
-                loss, train_op = sess.run([model.loss, model.train_op],
-                                          feed_dict={handle: train_handle, model.dropout: config.dropout})
+                # 每个step更新loss与
+                loss, train_op = sess.run([model.loss, model.train_op], feed_dict={handle: train_handle, model.dropout: config.dropout})
                 # 每到period，存储batch的loss信息
                 if global_step % config.period == 0:
                     loss_sum = tf.Summary(value=[tf.Summary.Value(tag="model/loss", simple_value=loss), ])
                     writer.add_summary(loss_sum, global_step)
                 # 每到checkpoint存当前参数
                 if global_step % config.checkpoint == 0:
-                    # 训练集的结果
-                    _, summ = evaluate_batch(model, config.val_num_batches, train_eval_file, sess,
-                                             "train", handle, train_handle)
+                    # 训练集的评估结果（多个batch）
+                    _, summ = evaluate_batch(model, config.val_num_batches, train_eval_file, sess, "train", handle, train_handle)
                     for s in summ:
                         writer.add_summary(s, global_step)
-                    # 验证集的结果
-                    metrics, summ = evaluate_batch(model, dev_total // config.batch_size + 1, dev_eval_file, sess,
-                                                   "dev", handle, dev_handle)
+                    
+                    # 验证集的评估结果（for early-stopping）
+                    metrics, summ = evaluate_batch(model, dev_total // config.batch_size + 1, dev_eval_file, sess, "dev", handle, dev_handle)
+                    for s in summ:
+                        writer.add_summary(s, global_step)
+                    writer.flush()
+                    # 判断停步条件
                     dev_f1 = metrics["f1"]
                     dev_em = metrics["exact_match"]
                     if dev_f1 < best_f1 and dev_em < best_em:
@@ -81,12 +84,9 @@ def train(config):
                         if patience > config.early_stop:
                             break
                     else:
-                        patience = 0
+                        patience = 0  # patience连续加10次才能停步，否则重头开始
                         best_em = max(best_em, dev_em)
                         best_f1 = max(best_f1, dev_f1)
-                    for s in summ:
-                        writer.add_summary(s, global_step)
-                    writer.flush()
                     # 存模型信息
                     filename = os.path.join(config.save_dir, "model_{}.ckpt".format(global_step))
                     saver.save(sess, filename)
@@ -101,30 +101,30 @@ def test(config):
         eval_file = json.load(fh)
     with open(config.test_meta, "r") as fh:
         meta = json.load(fh)
-    total = meta["total"]
+    test_total = meta["total"]
 
     graph = tf.Graph()
     print("Loading model...")
     with graph.as_default() as g:
         # 获取测试数据batch
-        test_batch = get_dataset(config.test_record_file,
-                                 get_record_parser(config, is_test=True), config).make_one_shot_iterator()
+        test_batch = get_dataset(config.test_record_file, get_record_parser(config, is_test=True), config).make_one_shot_iterator()
         # 生成模型结构
         model = Model(config, test_batch, word_mat, char_mat, trainable=False, graph=g)
         # 初始化配置
         sess_config = tf.ConfigProto(allow_soft_placement=True)
         sess_config.gpu_options.allow_growth = True
+        
         with tf.Session(config=sess_config) as sess:
             sess.run(tf.global_variables_initializer())
             saver = tf.train.Saver()
             saver.restore(sess, tf.train.latest_checkpoint(config.save_dir))
             if config.decay < 1.0:
                 sess.run(model.assign_vars)
-            # 预测数据
+            # 预测数据(一个batch预测)
             losses = []
             answer_dict = {}
             remapped_dict = {}
-            for _ in tqdm(range(total // config.batch_size + 1)):
+            for _ in tqdm(range(test_total // config.batch_size + 1)):
                 qa_id, loss, yp1, yp2 = sess.run([model.qa_id, model.loss, model.yp1, model.yp2])
                 answer_dict_, remapped_dict_ = convert_tokens(eval_file, qa_id.tolist(), yp1.tolist(), yp2.tolist())
                 answer_dict.update(answer_dict_)
