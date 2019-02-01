8378b21d59a40d8a3ca02aea773d28a161f7c79c
Kuyi King <kuyiking@gmail.com>
2019-02-01 20:27:42 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+
 import tensorflow as tf
-from layers import initializer, regularizer, residual_block, highway, conv, mask_logits, trilinear, total_params, optimized_trilinear_for_attention
+from layers import initializer, regularizer, highway, conv, residual_block, mask_logits, optimized_trilinear_for_attention, total_params
 
 
 class Model(object):
@@ -10,8 +12,7 @@ class Model(object):
         with self.graph.as_default():
             y1 = [[0, 0]]
             y2 = [[0, 0]]
-            self.global_step = tf.get_variable('global_step', shape=[], dtype=tf.int32,
-                                               initializer=tf.constant_initializer(0), trainable=False)
+            self.global_step = tf.get_variable('global_step', shape=[], dtype=tf.int32, initializer=tf.constant_initializer(0), trainable=False)
             self.dropout = tf.placeholder_with_default(0.0, (), name="dropout")
             if self.demo:
                 self.c = tf.placeholder(tf.int32, [1, None], "context")
@@ -69,35 +70,35 @@ class Model(object):
 
         with tf.variable_scope("Embedding_Encoder_Layer"):
             c = residual_block(c_emb,
-                               num_blocks = 1,
-                               num_conv_layers = 4,
-                               kernel_size = 7,
-                               mask = self.c_mask,
-                               num_filters = d,
-                               num_heads = nh,
-                               seq_len = self.c_len,
-                               scope = "Encoder_Residual_Block",
-                               bias = False,
-                               dropout = self.dropout)
+                               num_blocks=1,
+                               num_conv_layers=4,
+                               kernel_size=7,
+                               mask=self.c_mask,
+                               num_filters=d,
+                               num_heads=nh,
+                               seq_len=self.c_len,
+                               scope="Encoder_Residual_Block",
+                               bias=False,
+                               dropout=self.dropout)
             q = residual_block(q_emb,
-                               num_blocks = 1,
-                               num_conv_layers = 4,
-                               kernel_size = 7,
-                               mask = self.q_mask,
-                               num_filters = d,
-                               num_heads = nh,
-                               seq_len = self.q_len,
-                               scope = "Encoder_Residual_Block",
-                               reuse = True, # Share the weights between passage and question
-                               bias = False,
-                               dropout = self.dropout)
+                               num_blocks=1,
+                               num_conv_layers=4,
+                               kernel_size=7,
+                               mask=self.q_mask,
+                               num_filters=d,
+                               num_heads=nh,
+                               seq_len=self.q_len,
+                               scope="Encoder_Residual_Block",
+                               reuse=True, # Share the weights between passage and question
+                               bias=False,
+                               dropout=self.dropout)
 
         with tf.variable_scope("Context_to_Query_Attention_Layer"):
             S = optimized_trilinear_for_attention([c, q], self.c_maxlen, self.q_maxlen, input_keep_prob = 1.0 - self.dropout)
             mask_q = tf.expand_dims(self.q_mask, 1)
-            S_ = tf.nn.softmax(mask_logits(S, mask = mask_q))
+            S_ = tf.nn.softmax(mask_logits(S, mask=mask_q))
             mask_c = tf.expand_dims(self.c_mask, 2)
-            S_T = tf.transpose(tf.nn.softmax(mask_logits(S, mask = mask_c), dim = 1),(0,2,1))
+            S_T = tf.transpose(tf.nn.softmax(mask_logits(S, mask=mask_c), dim=1), (0, 2, 1))
             self.c2q = tf.matmul(S_, q)
             self.q2c = tf.matmul(tf.matmul(S_, S_T), c)
             attention_outputs = [c, self.c2q, c * self.c2q, c * self.q2c]
@@ -109,17 +110,17 @@ class Model(object):
                 if i % 2 == 0:  # dropout every 2 blocks
                     self.enc[i] = tf.nn.dropout(self.enc[i], 1.0 - self.dropout)
                 self.enc.append(residual_block(self.enc[i],
-                                               num_blocks = 7,
-                                               num_conv_layers = 2,
-                                               kernel_size = 5,
-                                               mask = self.c_mask,
-                                               num_filters = d,
-                                               num_heads = nh,
-                                               seq_len = self.c_len,
-                                               scope = "Model_Encoder",
-                                               bias = False,
-                                               reuse = True if i > 0 else None,
-                                               dropout = self.dropout))
+                                               num_blocks=7,
+                                               num_conv_layers=2,
+                                               kernel_size=5,
+                                               mask=self.c_mask,
+                                               num_filters=d,
+                                               num_heads=nh,
+                                               seq_len=self.c_len,
+                                               scope="Model_Encoder",
+                                               bias=False,
+                                               reuse=True if i > 0 else None,
+                                               dropout=self.dropout))
 
         with tf.variable_scope("Output_Layer"):
             start_logits = tf.squeeze(conv(tf.concat([self.enc[1], self.enc[2]],axis=-1), 1, bias=False, name="start_pointer"), -1)
