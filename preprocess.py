ab8c6f882ef7ea74853c420e39e793c01091b42a
Kuyi King <kuyiking@gmail.com>
2019-02-01 20:28:44 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -19,7 +19,6 @@ def _get_word(word, word2idx_dict):
 def preprocess(query, config, word2idx_dict, query_type=None):
     assert isinstance(query, str)
     seg_query = word_tokenize(query)
-
     # filter via limited length
     if query_type is "question":
         ques_limit = config.test_ques_limit
@@ -30,9 +29,8 @@ def preprocess(query, config, word2idx_dict, query_type=None):
 
     if len(seg_query) > ques_limit:
         return None, None
-
+    # generate indexes data
     ques_idxs = np.zeros([1, ques_limit], dtype=np.int32)
     for i, token in enumerate(seg_query):
         ques_idxs[0, i] = _get_word(token, word2idx_dict)
-
     return ques_idxs, seg_query
