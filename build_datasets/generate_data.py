812d7c2de5fa25dbd2f449063f05ce12b7873d58
Kuyi King <kuyiking@gmail.com>
2019-02-11 21:08:41 +0800

    Update generate_data.py

--- build_datasets/generate_data.py
+++ build_datasets/generate_data.py
@@ -20,7 +20,6 @@ new_sample = {'title': '',
                    }  # sample now
 """
 
-
 import json
 import random
 import pandas as pd
@@ -33,29 +32,14 @@ import pymongo
 # global initialization
 jieba.add_word("XXX")
 client = pymongo.MongoClient('127.0.0.1')  # open mongodb client
-table_raw = client['triples']['triples']  # open the raw database
+table_raw = client['triples2']['triples']  # open the raw database
 
 
 def split_train_and_test(load_path=None):
     if load_path is None:
-        lst_train = ["作者", "连载网站", "小说进度", "书名", "出生日期", "出版社", "职业", "国籍", "出生地", "中文学名",
-                     "性别", "公司名称", "民族", "外文名", "别名", "经营范围", "类型", "毕业院校", "总部地点", "作品名称",
-                     "酒店星级", "出版时间", "成立时间", "主要食材", "地理位置", "逝世日期", "面积", "地区", "创作年代",
-                     "餐馆名称", "所属地区", "类别", "主要成就", "行政区类别", "主要原料", "运动项目", "科", "地点", "游戏类型",
-                     "人口", "周围景观", "拼音", "公司性质", "拉丁学名", "创办时间", "种", "本名", "特点", "分布区域", "性质",
-                     "代表作品", "楼盘名", "属", "游戏大小", "释义", "主演", "籍贯", "占地面积", "时间", "字号", "分类", "解释",
-                     "副标题", "导演", "文学体裁", "药品名称", "地址", "位置", "英文名", "海拔", "产品类型", "人均价格", "简称",
-                     "隶属", "属性", "出处", "营业时间", "位于", "所处时代", "目", "口味", "建筑面积", "去世时间", "场上位置",
-                     "其他名称", "所属运动队", "辅料", "作用", "著名景点", "成立于", "外文名称", "大小", "政治面貌", "品牌",
-                     "下辖地区", "型号", "开发商", "制片地区", "原料", "始建于", "配料", "简介", "含义", "定义", "丛书", "主料",
-                     "上映时间", "歌曲原唱", "学历", "调料", "信仰", "编剧", "物业类别", "用途", "年平均气温", "材料",
-                     "气候条件", "屏幕尺寸", "属于", "纲", "房间数量", "目的", "出品时间", "内容", "实质", "对象"]
-        lst_test = ["耕地面积", "组成", "功能", "楼盘地址", "英文名称", "登场作品", "填词", "国家", "年代", "优点", "谱曲",
-                    "应用", "员工数", "门", "所属专辑", "公司口号", "版次", "译者", "功效", "发行时间", "上市时间", "原版名称",
-                    "主要院系", "主要奖项", "出品公司", "注音", "界", "装帧", "页数", "定价", "宗旨", "身高", "CPU型号",
-                    "亚科", "主要营养成分", "编曲", "集数", "校训", "适宜人群", "游戏平台", "体重", "酒店地址", "发行日期",
-                    "开本", "其它译名", "系列", "制片人", "语言", "亚目", "尺寸", "音乐风格", "发行公司", "邮政区码", "亚门",
-                    "血型", "歌曲语言", "星座", "族"]
+        # 此处省略完整列表
+        lst_train = ["出生日期", "职业", "国籍", "出生地", "性别"]
+        lst_test = ["国家", "年代", "星座", "族"]
         return lst_train, lst_test
     else:
         raise ValueError("No training and test relations information!")
@@ -175,9 +159,9 @@ def gen_data(lst, dict_q, n=100, ratio=-1.0):
                     lst_test.append(d_new)
                     c_test += 1
     # save data
-    save_data(lst_train, "train.json")
-    save_data(lst_dev, "dev.json")
-    save_data(lst_test, "test.json")
+    save_data(lst_train, "train_v2.json")
+    save_data(lst_dev, "dev_v2.json")
+    save_data(lst_test, "test_v2.json")
     print("Dataset Size: Training-{}, Dev-{}, Test-{}.".format(c_train, c_dev, c_test))
     return None
 
